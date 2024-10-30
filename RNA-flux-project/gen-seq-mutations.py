6ddbeb9d85e4e54e48aef1a3a3c399fba6cc5400
YMWani <yashraj.wani@princeton.edu>
2024-10-30 16:00:00 -0400

    created a new python file

---
+++ RNA-flux-project/gen-seq-mutations.py
@@ -0,0 +1,9 @@
+import numpy as np
+import argparse
+
+"""
+Script that takes a sequence as an input parameter and mutates each residue with another residue
+with a given probability.
+
+Arguments:
+"""
