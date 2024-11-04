52ed0ab7f8e5e07af9dd8fe3b1d83056d2b18750
Yashraj <yashraj.wani@princeton.edu>
2024-11-04 14:24:25 -0500

    Added fucntions to adjust density profiles and to measure fitness of a multiphasic condensate.

--- RNA_flux_project/utils.py
+++ RNA_flux_project/utils.py
@@ -1,6 +1,10 @@
 import json
 import random
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+plt.style.use("../plotting/ymw.mplstyle")
 
 
 def check_sequence_validity(sequence):
@@ -166,6 +170,153 @@ def gen_mixture_chain_config(sequence1, sequence2, outfile="config.dat"):
     out.close()
 
 
+def parse_density_profile_file(filename):
+    """
+    This function reads the density profile file created from LAMMPS simulations using the 
+    "compute .. chunk/atom ..." command and extracts the timesteps and density profiles at
+    the different time points
+    
+    Parameters:
+    - filename (str): Name of the file containing the density profile
+
+    Output:
+    - timesteps (numpy array): Timesteps at which the density profile was measured and saved
+    - densities (numpy array): Density profiles at every timestep
+    """    
+    with open(filename, 'r') as data:
+        densities = []
+        chunk_data = []
+        timesteps = []
+        for line in data:
+            line = line.strip() # Remove leading/trailing whitespace
+            parts = line.split() # Split the line to get the different components
+
+            if line.startswith('#'):
+                # Skip comment lines
+                continue
+            
+            elif (len(parts) == 3):
+                # Line with 3 components: (Timestep, Number-of-bins, Total-count)
+                timesteps.append(int(parts[0]))
+                if (bool(chunk_data)):
+                    densities.append(chunk_data)
+                    chunk_data = []
+                
+            else:
+                # Line with 4 components: (bin, Coord1, Ncount, density/mass)
+                chunk_data.append([float(parts[1]), float(parts[3])])
+        densities.append(chunk_data)
+    timesteps = np.array(timesteps)
+    densities = np.array(densities)
+    return timesteps, densities
+
+
+def compute_averaged_density_profile(density_prof):
+    """
+    This function takes the raw density profile extracted from the LAMMPS density profile file
+    and averages it over all the frames. In addition, the density profile is also recentered 
+    such that COM of condensate lies at 0.5 (reduced units)
+
+    Parameters:
+    - density_prof (numpy array): raw density profile obtained from LAMMPS simulations.
+                                  Expected shape (nframes, nbins, 2)
+                                  The final 2 columns are just the bin_centers and bin_density
+
+    Output:
+    - bins: bin centers
+    - bin_density: bin densities
+    """
+    avg_density_prof = np.mean(density_prof, axis=0) # Assuming no noise in the data, so we don't discard any part of it
+    
+    # Shift the density profile such that it is centered.
+    # Find the center of mass of the density profile
+    bins = avg_density_prof[:,0]
+    bin_density = avg_density_prof[:,1]
+    cm = np.sum(bins * bin_density) / np.sum(bin_density)
+    # Adjust the bin positions such that the COM is at the center of the box (i.e. 0.5)
+    bins -= cm - 0.5
+    bins[bins < 0] += 1.0
+    
+    return bins, bin_density
+
+
+def find_interfaces(coords, avg_profile, derivative_threshold=1e-5, percentile_low=15, percentile_high=85, min_dilute_size_multiplier=0.3):
+    """
+    This function takes averaged density profile for a direct coexistence simulations
+    and fits a super gaussian function to the profile. From the second derivative of the 
+    super gaussian one can extract the interface coordinates of the slab.
+
+    Parameters:
+    - coords (array): bin_centers of the density profile
+    - avg_profile (array): bin_densities of the density profile
+
+    Output:
+    - left_interface_coord
+    - right_interface_coord
+    - fine_coords: finely spaced bin centers
+    - fitted_profile: Fitted profile on the finely spaced bin centers
+    """
+    # fit super gaussian
+    super_gaussian = lambda x, A, x0, sigma, p: A*np.exp(-2*((x-x0)/sigma)**(2*np.round(p)))
+    initial_guess = [np.percentile(avg_profile, 80), np.mean(coords), np.std(coords), 2]
+    popt, pcov = curve_fit(super_gaussian, coords, avg_profile, p0=initial_guess, maxfev=10000)
+    A, x0, sigma, p = popt
+    P = 2*np.round(p)
+    fine_coords = np.linspace(min(coords), max(coords), num=1000)
+    fitted_profile = super_gaussian(fine_coords, *popt)
+    # compute second derivative
+    first_derivative = np.gradient(fitted_profile)
+    second_derivative = np.gradient(first_derivative)
+    # binarize normalized second derivatives with derivative threshold and get interfaces
+    threshold = derivative_threshold # units: (density per length per length) / length
+    binarized = [abs(x)/max(fitted_profile) <= threshold for x in second_derivative]
+    num_clusters = 1
+    ## keep track of "True" cluster boundaries for plotting
+    true_cluster_boundaries = []
+    curr_cluster = [0] if binarized[0] else []
+    for idx in range(1, len(binarized)):
+        if binarized[idx] != binarized[idx-1]:
+            num_clusters += 1
+            if binarized[idx]:
+                curr_cluster.append(fine_coords[idx])
+            else:
+                curr_cluster.append(fine_coords[idx-1])
+                true_cluster_boundaries.append(curr_cluster)
+                curr_cluster = []
+    if len(curr_cluster) == 1: # ends on True cluster that doesn't get closed in loop, which will be common
+        curr_cluster.append(fine_coords[-1])
+        true_cluster_boundaries.append(curr_cluster)
+    num_clusters += 1
+    num_clusters /= 2
+    if binarized[0]==True and binarized[-1]==True and num_clusters == 5: # clean condensate; zero second derivative on ends, slope up/down and the middle
+        left_interface_coord = fine_coords[round(([idx for idx in range(1, len(binarized)) if binarized[idx] == False and binarized[idx-1] == True][1] + [idx for idx in range(1, len(binarized)) if binarized[idx] == True and binarized[idx-1] == False][0])/2)]
+        right_interface_coord = fine_coords[round(([idx for idx in list(reversed(range(0, len(binarized)-2))) if binarized[idx] == False and binarized[idx+1] == True][1] + [idx for idx in list(reversed(range(0, len(binarized)-2))) if binarized[idx] == True and binarized[idx+1] == False][0])/2)]
+        if (left_interface_coord + (max(fine_coords)-right_interface_coord)) <= min_dilute_size_multiplier * right_interface_coord - left_interface_coord: # ensure dilute phase isn't too small
+            left_interface_coord, right_interface_coord = np.percentile(coords, percentile_low), np.percentile(coords, percentile_high)
+    else:
+        left_interface_coord, right_interface_coord = np.percentile(coords, percentile_low), np.percentile(coords, percentile_high)
+    
+    return left_interface_coord, right_interface_coord, fine_coords, fitted_profile
+
+
+def evaluate_fitness(rhoA_center, rhoB_center, rhoA_vapour, rhoB_vapour, s):
+    """
+    Evaluate fitness of a multiphasic condensate.
+
+    Parameters:
+    - rhoA_center (float): density of protein A in the center of the condensate (reduced units)
+    - rhoB_center (float): density of protein B in the center of the condensate (reduced units)
+    - rhoA_vapour (float): density of protein A in the vapour phase (reduced units)
+    - rhoB_vapour (float): density of protein B in the vapour phase (reduced units)
+    - s (float): Weighting parameter
+
+    Output:
+    - fitness (float)
+    """
+    return abs(rhoA_center - rhoB_center) - s*(rhoA_vapour + rhoB_vapour)
+
+
+
 
 
 if __name__ == "__main__":
@@ -175,8 +326,8 @@ if __name__ == "__main__":
     in a simulation box. The configuration needs to be equilibrated through minimization/running a
     simulation for some time.
     """
-    seq1 = "AFAAF"*10
-    check_sequence_validity(seq1)
+    # seq1 = "AFAAF"*10
+    # check_sequence_validity(seq1)
     
     # seq2 = "F"*50
     # check_sequence_validity(seq2)
@@ -185,3 +336,49 @@ if __name__ == "__main__":
     
     # gen_mixture_chain_config(seq1, seq2, "initialConfig.dat")
     
+    """                            ****** SECTION *****
+    """
+    # Global density profile
+    tsteps, density_profile = parse_density_profile_file("/Users/yw9071_admin/Downloads/density_all.dat")
+    bins, bin_density = compute_averaged_density_profile(density_profile)
+    # Visualization
+    fig, ax = plt.subplots()
+    # Recentered density profile
+    ax.plot(bins, bin_density, marker='o', linestyle='')
+    left_intrfc, right_intrfc, fine_coords, fitted_profile = find_interfaces(bins, bin_density)
+    ax.plot(fine_coords, fitted_profile)
+    # Interfaces
+    yvals = np.linspace(min(bin_density), max(bin_density), 3)
+    ax.plot(np.repeat(left_intrfc, 3), yvals, color="black", linestyle="dashed")
+    ax.plot(np.repeat(right_intrfc, 3), yvals, color="black", linestyle="dashed")
+    # Set labels
+    ax.set_xlabel(r"$z/L_\mathrm{z}$")
+    ax.set_ylabel(r"$\rho \, (\mathrm{g/cm^3})$")
+    plt.tight_layout()
+    # plt.show()
+
+    # Protein specific density profile
+    tsteps_I, density_profile_I = parse_density_profile_file("/Users/yw9071_admin/Downloads/density_inner.dat")
+    bins_I, bin_density_I = compute_averaged_density_profile(density_profile_I)
+    
+    tsteps_O, density_profile_O = parse_density_profile_file("/Users/yw9071_admin/Downloads/density_outer.dat")
+    bins_O, bin_density_O = compute_averaged_density_profile(density_profile_O)
+    
+    # Recentered density profile
+    ax.plot(bins_I, bin_density_I, label="Inner protein")
+    ax.plot(bins_O, bin_density_O, label="Outer protein")
+    plt.legend()
+    plt.show()
+
+    mask_bins_O = (bins_O >= 0.45) & (bins_O <= 0.55)
+    rhoO_center = np.mean(bin_density_O[mask_bins_O])
+    mask_bins_I = (bins_I >= 0.45) & (bins_I <= 0.55)
+    rhoI_center = np.mean(bin_density_I[mask_bins_I])
+
+    mask_vapour_O = (bin_density_O < 0.05)
+    rhoO_vapour = np.mean(bin_density_O[mask_vapour_O])
+    mask_vapour_I = (bin_density_I < 0.05)
+    rhoI_vapour = np.mean(bin_density_I[mask_vapour_I])
+    
+    fitness = evaluate_fitness(rhoO_center, rhoI_center, rhoO_vapour, rhoI_vapour, s=0)
+    print(fitness)
