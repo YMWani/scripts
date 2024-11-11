6a5520005ee8a4e266e5ca94015353fbfc0db347
Yashraj <yashraj.wani@princeton.edu>
2024-11-11 11:26:02 -0500

    added function to utils.py in tau_protein_project

--- tau_protein_project/utils.py
+++ tau_protein_project/utils.py
@@ -32,3 +32,26 @@ def compute_SCD(seq):
     
     return SCD
 
+def estimate_num_PEG_chains(phi, v_box=4e7, Nm_PEG=182, sigma_PEG=4.644, r0_PEG=2.322):
+    """
+    Estimate the number of PEG chains required in the simulations given a volume fraction (phi)
+
+    Parameters:
+    - phi (float): Volume fraction
+    """
+    # First we need to compute the effective volume of a single PEG chain
+    # NOTE: There is a overlap between consecutive monomers.
+    vol_overlap = (Nm_PEG-1) * overlap_volume_two_spheres(sigma_PEG/2., r0_PEG)
+    vol_monomer = (np.pi/6.) * sigma_PEG**3
+    vol_PEG = Nm_PEG * vol_monomer - vol_overlap
+
+    # Work backwords given that we know the required volume fraction
+    num_chains = int(np.round(phi * v_box / vol_PEG))
+    return num_chains
+
+def overlap_volume_two_spheres(R, d):
+    return (np.pi/12.) * (4*R + d) * (2*R - d)
+
+if __name__ == "__main__":
+    num_PEG_chains = estimate_num_PEG_chains(phi=0.025)
+    print(num_PEG_chains)
