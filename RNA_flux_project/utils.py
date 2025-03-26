deafe342a9e8f51fe383bd6074ab38fa36b4d8c3
Yashraj <yashraj.wani@princeton.edu>
2025-03-26 16:17:08 -0400

    debugging

--- RNA_flux_project/utils.py
+++ RNA_flux_project/utils.py
@@ -2089,6 +2089,7 @@ def monte_carlo_insert_single_chain(box_bounds, existing_positions, monomers_per
     z_min, z_max = box_bounds[2]
     
     for _ in range(max_chain_tries):
+        print(_)
         # Start with a random position within the simulation box bounds
         # NOTE: Cavity should not have a periodic boundary in the z direction. But I still keep it here
         # since the overlap with cavity monomers should do the job of eleminating the periodic boundary issue.
