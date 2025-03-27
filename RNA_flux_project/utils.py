aed096250f187f41d40d4db08e85f20b1b21bd0c
Yashraj <yashraj.wani@princeton.edu>
2025-03-27 13:17:10 -0400

    debugging

--- RNA_flux_project/utils.py
+++ RNA_flux_project/utils.py
@@ -1403,13 +1403,13 @@ def place_chains_in_confinement(seq, box_bounds, nchains):
     seq_length = len(seq)*r0 + 1.0 # Approximate excluded length of chain with buffer
     print("Sequence length: ", seq_length)
     Lx = box_bounds[0][1] - box_bounds[0][0]
-    print("Lx, Ly, Lz: ", Lx, Ly, Lz)
     nx = int(Lx//seq_length)
     Ly = box_bounds[1][1] - box_bounds[1][0]
     ny = int(Ly//seq_length)
     Lz = box_bounds[2][1] - box_bounds[2][0]
     nz = int(Lz//seq_length)
     buffer = 5.0 # buffer distance between chains
+    print("Lx, Ly, Lz: ", Lx, Ly, Lz)
     print("nx, ny, nz:", nx, ny, nz)
     box_origin = [box_bounds[0][0], box_bounds[1][0], box_bounds[2][0]]
     # Extend chains along x direction if the x dimension is largest
