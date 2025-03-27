831f2b5ecbcb4b96e4c8e200859b351e7be3773f
Yashraj <yashraj.wani@princeton.edu>
2025-03-27 13:12:24 -0400

    debugging

--- RNA_flux_project/utils.py
+++ RNA_flux_project/utils.py
@@ -1399,7 +1399,6 @@ def place_chains_in_confinement(seq, box_bounds, nchains):
     - position_data (list of tuples): (atom_id, mol_id, atom_type, atom_charge, x, y, z)
     - bond_data (list of tuples): (bond_id, bond_type, atom_id_1, atom_id_2)
     """
-    print(nchains)
     r0 = 3.81 # equilibrium bond length (Angstroms)
     seq_length = len(seq)*r0 + 8.0 # Approximate excluded length of chain with buffer
     Lx = box_bounds[0][1] - box_bounds[0][0]
@@ -1452,7 +1451,8 @@ def place_chains_in_confinement(seq, box_bounds, nchains):
                         lattice_points.append([box_origin[0]+x_pos, box_origin[1]+y_pos, box_origin[2]+z_pos])
     
     lattice_points = np.array(lattice_points) # lattice points for chains
-
+    print(lattice_points)
+    
     # Randomly choose nchains lattice points from the ones generated to place the chains
     random_indices = np.random.choice(np.arange(0, len(lattice_points)), nchains, replace=False)
     chosen_lattice_points = lattice_points[random_indices]
