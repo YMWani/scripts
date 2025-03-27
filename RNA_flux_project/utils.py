e708d48112fe015004bd94a986deeb6bc664ed33
Yashraj <yashraj.wani@princeton.edu>
2025-03-27 13:15:13 -0400

    debugging

--- RNA_flux_project/utils.py
+++ RNA_flux_project/utils.py
@@ -1408,6 +1408,7 @@ def place_chains_in_confinement(seq, box_bounds, nchains):
     Lz = box_bounds[2][1] - box_bounds[2][0]
     nz = int(Lz//seq_length)
     buffer = 5.0 # buffer distance between chains
+    print(nx, ny, nz)
     box_origin = [box_bounds[0][0], box_bounds[1][0], box_bounds[2][0]]
     # Extend chains along x direction if the x dimension is largest
     lattice_points = []
