08dfa14aa658935b1cd1e116b3407f944f119171
Yashraj <yashraj.wani@princeton.edu>
2025-05-04 15:38:26 -0400

    debugging

--- RNA_flux_project/analyze_Rg.py
+++ RNA_flux_project/analyze_Rg.py
@@ -211,6 +211,9 @@ def compute_gyration_tensor(com_pos, atom_positions, chain_masses):
     asphericities = np.zeros(nchains) # Create empty array for storing apshericity of the chains
     for i in range(nchains):
         relative_pos = atom_positions[i] - com_pos[i] # shape: [#atoms-per-chain, 3]
+        print(relative_pos)
+        print(np.repeat(chain_masses,3).reshape((-1,3))*relative_pos)
+        exit()
         gyration_tensor = np.einsum("jm,jn->mn", chain_masses*relative_pos, relative_pos)/relative_pos.shape[0]
         eig_vals, eig_vecs = np.linalg.eig(gyration_tensor)
         eig_vals_sorted = np.sort(eig_vals)
@@ -241,8 +244,6 @@ if __name__=="__main__":
                                           atom_positions.shape[1]//Nm,
                                           Nm,
                                           atom_positions.shape[2])) # [#frames, #chains, #atoms-per-chain, 3]
-    print(atom_positions_reshaped)
-    exit()
     # Get the mass of chain monomers using particle types
     chain_masses = extract_particle_masses(atom_types)
     # Compute center of mass positions of the chains at every timestep
