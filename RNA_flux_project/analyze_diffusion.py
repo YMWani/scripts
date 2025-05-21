a4d321986a8b04ecd2a2af61fb6450611c2a6b83
Yashraj <yashraj.wani@princeton.edu>
2025-05-21 11:47:25 -0400

    debugging

--- RNA_flux_project/analyze_diffusion.py
+++ RNA_flux_project/analyze_diffusion.py
@@ -361,7 +361,7 @@ if __name__=="__main__":
         # print(f"\nAnalyzing chain {i+1}")
         chain_traj = com_positions[:,i,:] # [#frames, 3] (unwrapped coordinates)
         chain_traj_wrapped = com_positions_wrapped[:,i,:] # (wrapped coordinates)
-
+        print(chain_traj.shape)
 
         """ 1. Average time for a chain to completely flux through the condensate"""
         # Array to store information about chain location in the simulation box
