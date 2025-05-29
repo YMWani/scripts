152ffe6b18da6b01755faaf6d2a27213e8172bc8
Yashraj <yashraj.wani@princeton.edu>
2025-05-29 15:45:35 -0400

    debugging finished

--- RNA_flux_project/utils.py
+++ RNA_flux_project/utils.py
@@ -1982,9 +1982,6 @@ def find_interfaces(coords, avg_profile):
                            sigma=sigma_, absolute_sigma=True, maxfev=10000)
     A, x0, sigma, p = popt
 
-    print(initial_guess)
-    print(popt)
-
     P = 2*np.round(p)
     fine_coords = np.linspace(min(coords), max(coords), num=1000)
     fitted_profile = super_gaussian(fine_coords, *popt)
