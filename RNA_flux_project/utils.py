225f112ef369b61b2ff6f1387b05e7bc09e08bc6
Yashraj <yashraj.wani@princeton.edu>
2026-01-06 16:45:19 -0500

    Added function to add two protein slabs next to spherical cavity

--- RNA_flux_project/utils.py
+++ RNA_flux_project/utils.py
@@ -1001,6 +1001,163 @@ def write_spherical_cavity_with_protein1_config(simBox, cavity_positions, cavity
     configFile.close()
 
 
+def write_spherical_cavity_with_protein1_config_2(simBox, cavity_positions, cavity_types, protein_coords1=None,
+                        protein_bond_data1=None, protein_coords2=None, protein_bond_data2=None, num_atom_types = 40):
+    """
+    Create a initial config file for a spherical cavity with two compressed protein slabs on opposite sides of it.
+
+    Arguments:
+    - simBox (float list): List of the lower and upper bounds for the simulation box
+    - cavity_positions (float array): Positions of the monomers of the spherical cavity
+    - cavity_types (char list): List of the atom_types of the surface monomers.
+    - protein_coords1 (tuple list): A list of tuples containing details for each atom of the first protein slab (positive z)
+                                  (atom_id, mol_id, atom_type, atom_charge, xcoord, ycoord, zcoord)
+    - protein_bond_data1 (list of tuples): Bond data for the first protein slab
+                                         (bond_id, bond_type, first_atom_id, second_atom_id)
+    - protein_coords2 (tuple list): A list of tuples containing details for each atom of the second protein slab (negative z)
+                                  (atom_id, mol_id, atom_type, atom_charge, xcoord, ycoord, zcoord)
+    - protein_bond_data2 (list of tuples): Bond data for the second protein slab
+                                         (bond_id, bond_type, first_atom_id, second_atom_id)
+    
+    Returns: config file for LAMMPS simulation - "cavity_protein1_config.dat"
+    """
+    parent_dir = Path(__file__).parent
+    with open(f'{parent_dir}/amino_acid_dict.json', 'r') as f:
+        aa_dict = json.load(f)
+    
+    configFile = open('cavity_protein1_config.dat','w')
+    configFile.write('LAMMPS data file with spherical cavity and two protein slabs\n\n')
+
+    # Overall data
+    num_atoms = cavity_positions.shape[0]
+    if protein_coords1 is not None:
+        num_atoms += len(protein_coords1)
+    if protein_coords2 is not None:
+        num_atoms += len(protein_coords2)
+    configFile.write(f'{num_atoms} atoms\n')
+    configFile.write(f'{num_atom_types} atom types\n')
+    
+    num_bonds = 0
+    if protein_bond_data1 is not None:
+        num_bonds += len(protein_bond_data1)
+    if protein_bond_data2 is not None:
+        num_bonds += len(protein_bond_data2)
+    configFile.write(f'{num_bonds} bonds\n')
+    configFile.write('1 bond types\n\n')
+
+    # Simulation box
+    configFile.write('%5f   %5f  xlo xhi\n'%(simBox[0][0],simBox[0][1]))
+    configFile.write('%5f   %5f  ylo yhi\n'%(simBox[1][0],simBox[1][1]))
+    configFile.write('%5f   %5f  zlo zhi\n\n'%(simBox[2][0],simBox[2][1]))
+    
+    # Particle masses
+    configFile.write('Masses\n\n')
+    # First assign ids and masses for Amino acids for protein chains 
+    for key, value in aa_dict.items():
+        configFile.write(f"{value.get('id')} {value.get('mass')}\n")
+    # Second, assign ids and masses for Amino acids for cavity monomers 
+    for key, value in aa_dict.items():
+        configFile.write(f"{value.get('id')+20} {value.get('mass')}\n")
+
+    # Particle positions
+    configFile.write('\nAtoms\n\n')
+
+    # First assign positions of the cavity
+    mol_id = 1 # Let's assign the cavity as mol_id = 1
+    for index, pos in enumerate(cavity_positions):
+        atom_id = index + 1
+        atom_type = aa_dict[cavity_types[index]]["id"] + 20 # Add 20 for cavity monomers
+        atom_charge = aa_dict[cavity_types[index]]["charge"]
+        # By construction, the cavity positions are such that the center of the cavity is at (0.,0.,0.)
+        # We need to recenter it to the center of the new simulation box
+        xcoord = pos[0] + (simBox[0][0] + (simBox[0][1] - simBox[0][0])/2.)
+        ycoord = pos[1] + (simBox[1][0] + (simBox[1][1] - simBox[1][0])/2.)
+        zcoord = pos[2] + (simBox[2][0] + (simBox[2][1] - simBox[2][0])/2.)
+        configFile.write('%d %d %d  %f %f  %f  %f\n' %(atom_id, mol_id, atom_type, atom_charge, xcoord, ycoord, zcoord))
+    
+    num_cavity_atoms = cavity_positions.shape[0]
+    current_atom_offset = num_cavity_atoms
+    current_mol_offset = 1  # Cavity is mol_id 1
+    
+    Lx = simBox[0][1] - simBox[0][0]
+    Ly = simBox[1][1] - simBox[1][0]
+    Lz = simBox[2][1] - simBox[2][0]
+    
+    # If first protein slab positions are given, place them on the positive z side
+    if protein_coords1 is not None:
+        # Wrap the coordinates in the simulation box
+        wrapped_coords1 = []
+        for atom_id, mol_id, atom_type, atom_charge, xcoord, ycoord, zcoord in protein_coords1:
+            xw = xcoord - (xcoord//Lx)*Lx
+            yw = ycoord - (ycoord//Ly)*Ly
+            zw = zcoord - (zcoord//Lz)*Lz
+            wrapped_coords1.append([xw, yw, zw])
+        wrapped_coords1 = np.array(wrapped_coords1)
+        
+        # Calculate the shift to place the first protein slab on positive z side of the cavity
+        delta_x1 = (simBox[0][0] + simBox[0][1])/2. - np.mean(wrapped_coords1[:,0])
+        delta_y1 = (simBox[1][0] + simBox[1][1])/2. - np.mean(wrapped_coords1[:,1])
+        delta_z1 = np.max(cavity_positions[:,2]) + Lz/2. - np.min(wrapped_coords1[:,2]) + 1.0 # Buffer of 1.0 Angstroms
+
+        # Write protein 1 atoms
+        for atom_id, mol_id, atom_type, atom_charge, xcoord, ycoord, zcoord in protein_coords1:
+            configFile.write('%d %d %d  %f %f  %f  %f\n' %(atom_id+current_atom_offset, mol_id+current_mol_offset, 
+                                                           atom_type, atom_charge, xcoord+delta_x1, ycoord+delta_y1, zcoord+delta_z1))
+        
+        # Update offsets for the next protein slab
+        current_atom_offset += len(protein_coords1)
+        # Find the maximum mol_id in protein_coords1
+        max_mol_id1 = max([mol_id for _, mol_id, _, _, _, _, _ in protein_coords1])
+        current_mol_offset += max_mol_id1
+    
+    # If second protein slab positions are given, place them on the negative z side
+    if protein_coords2 is not None:
+        # Wrap the coordinates in the simulation box
+        wrapped_coords2 = []
+        for atom_id, mol_id, atom_type, atom_charge, xcoord, ycoord, zcoord in protein_coords2:
+            xw = xcoord - (xcoord//Lx)*Lx
+            yw = ycoord - (ycoord//Ly)*Ly
+            zw = zcoord - (zcoord//Lz)*Lz
+            wrapped_coords2.append([xw, yw, zw])
+        wrapped_coords2 = np.array(wrapped_coords2)
+        
+        # Calculate the shift to place the second protein slab on negative z side of the cavity
+        delta_x2 = (simBox[0][0] + simBox[0][1])/2. - np.mean(wrapped_coords2[:,0])
+        delta_y2 = (simBox[1][0] + simBox[1][1])/2. - np.mean(wrapped_coords2[:,1])
+        delta_z2 = np.min(cavity_positions[:,2]) - Lz/2. - np.max(wrapped_coords2[:,2]) - 1.0 # Buffer of 1.0 Angstroms
+
+        # Write protein 2 atoms
+        for atom_id, mol_id, atom_type, atom_charge, xcoord, ycoord, zcoord in protein_coords2:
+            configFile.write('%d %d %d  %f %f  %f  %f\n' %(atom_id+current_atom_offset, mol_id+current_mol_offset, 
+                                                           atom_type, atom_charge, xcoord+delta_x2, ycoord+delta_y2, zcoord+delta_z2))
+    
+    # Bond data
+    # Only the proteins will have bonds
+    if protein_bond_data1 is not None or protein_bond_data2 is not None:
+        configFile.write('\nBonds\n\n')
+        
+        current_bond_id = 0
+        atom_offset = num_cavity_atoms
+        
+        # Write bonds for first protein slab
+        if protein_bond_data1 is not None:
+            for bond_id, bond_type, first_atom_id, second_atom_id in protein_bond_data1:
+                current_bond_id += 1
+                configFile.write('%d %d %d %d\n' %(current_bond_id, bond_type, 
+                                                   first_atom_id+atom_offset, second_atom_id+atom_offset))
+            atom_offset += len(protein_coords1)
+        
+        # Write bonds for second protein slab
+        if protein_bond_data2 is not None:
+            for bond_id, bond_type, first_atom_id, second_atom_id in protein_bond_data2:
+                current_bond_id += 1
+                configFile.write('%d %d %d %d\n' %(current_bond_id, bond_type, 
+                                                   first_atom_id+atom_offset, second_atom_id+atom_offset))
+    
+    # Close file
+    configFile.close()
+
+
 def write_cavity_with_protein1_config(simBox, cavity_positions, cavity_types, protein_coords=None,
                         protein_bond_data=None, num_atom_types = 40):
     """
